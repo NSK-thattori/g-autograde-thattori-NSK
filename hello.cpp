1d7c635be00de7bb09c1768e3bcc1f12af741640
TAKUMI HATTORI <148437024+thattori-NSK@users.noreply.github.com>
2025-05-14 11:42:40 +0900

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+
 int Factorial( int number ) {
   return number <= 1 ? 1 : Factorial( number - 1 ) * number;
 }
